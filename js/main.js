c1088f077a403c15173d0825b64b3aec5dc81ddb
Aleksander <sanya.pyatkov.89@mail.ru>
2021-12-16 01:12:07 +0600

    quasar
    quasar

--- js/main.js
+++ js/main.js
@@ -141,7 +141,7 @@ new Swiper('.DAO-is-changing__slider', {
       },
       801:{
         slidesPerView: 3,
-        spaceBetween:64,
+        spaceBetween:54,
       },
       1501:{
         slidesPerView: 2,
@@ -153,31 +153,44 @@ new Swiper('.DAO-is-changing__slider', {
 });
 
 
+/* ------------------------------ economic-here ----------------------------- */
 
-let ecoClick = document.querySelector('.economic-here');
-let eco1 = document.querySelector('.block1')
-let eco2 = document.querySelector('.block2')
-let eco3 = document.querySelector('.block3')
-let eco4 = document.querySelector('.block4')
-ecoClick.addEventListener('click', function(event){
-  if (document.documentElement.clientWidth < 1080) {
-  
-  if(eco4.classList.contains('active')){
-    eco4.classList.remove('active');
-    eco1.classList.add('active');
-  }else if(eco1.classList.contains('active')){
-    eco1.classList.remove('active');
-    eco2.classList.add('active');
-  }else if(eco2.classList.contains('active')){
-    eco2.classList.remove('active');
-    eco3.classList.add('active');
-  }else if(eco3.classList.contains('active')){
-    eco3.classList.remove('active');
-    eco4.classList.add('active');
+let vid = document.getElementById("myVideo");
+let sdf = document.querySelector('.economic-here__video-circle');
+
+  sdf.addEventListener('mouseover', function(){
+    vid.play();
+  })
+  sdf.onmouseout = function(){
+  vid.load();
   }
 
-}
-});
+
+
+// let ecoClick = document.querySelector('.economic-here');
+// let eco1 = document.querySelector('.block1')
+// let eco2 = document.querySelector('.block2')
+// let eco3 = document.querySelector('.block3')
+// let eco4 = document.querySelector('.block4')
+// ecoClick.addEventListener('click', function(event){
+//   if (document.documentElement.clientWidth < 1080) {
+  
+//   if(eco4.classList.contains('active')){
+//     eco4.classList.remove('active');
+//     eco1.classList.add('active');
+//   }else if(eco1.classList.contains('active')){
+//     eco1.classList.remove('active');
+//     eco2.classList.add('active');
+//   }else if(eco2.classList.contains('active')){
+//     eco2.classList.remove('active');
+//     eco3.classList.add('active');
+//   }else if(eco3.classList.contains('active')){
+//     eco3.classList.remove('active');
+//     eco4.classList.add('active');
+//   }
+
+// }
+// });
 
 
 
